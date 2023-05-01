73c97a7c98f85c6ce2d334e0ac6001eb53ae082a
Marco Marchini <775639+marcom04@users.noreply.github.com>
2023-05-01 20:29:48 +0200

    Fix react copy toast.

--- react/frontend/src/components/ShortenForm.js
+++ react/frontend/src/components/ShortenForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Container, Form, Button, InputGroup, Toast } from 'react-bootstrap';
-import { format } from 'date-fns';
+import {Container, Form, Button, InputGroup, Toast, ToastContainer} from 'react-bootstrap';
+import {format} from 'date-fns';
 import axios from 'axios';
 
-import { GUEST_SHORTEN_API_URL } from '../constants';
+import {GUEST_SHORTEN_API_URL} from '../constants';
 
 class ShortenForm extends React.Component {
     state = {
@@ -24,7 +24,7 @@ class ShortenForm extends React.Component {
     }
 
     onChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
+        this.setState({[e.target.name]: e.target.value});
     };
 
     resetState = () => {
@@ -41,11 +41,11 @@ class ShortenForm extends React.Component {
         e.preventDefault();
         axios.post(GUEST_SHORTEN_API_URL, {target: this.state.target}).then(response => {
             this.setState({
-               short_url: response.data.short_url,
-               expiry_date: new Date(response.data.expiry_date),
-               shorten_disabled: true,
-               short_url_hidden: false
-           });
+                short_url: response.data.short_url,
+                expiry_date: new Date(response.data.expiry_date),
+                shorten_disabled: true,
+                short_url_hidden: false
+            });
         }).catch(err => {
             console.log(err);
         });
@@ -53,69 +53,78 @@ class ShortenForm extends React.Component {
 
     render() {
         return (
-            <Container>
-                <Form onSubmit={this.createMapping}>
-                    <Form.Group className="mb-3">
-                        <Form.Label htmlFor="target">Target URL</Form.Label>
-                        <Form.Control
-                            name="target"
-                            type="url"
-                            required
-                            placeholder="Enter the URL to shorten..."
-                            value={this.state.target}
-                            onChange={this.onChange}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" hidden={this.state.short_url_hidden}>
-                        <Form.Label htmlFor="short_url">
-                            Here's your shortened URL, valid until: {format(this.state.expiry_date, 'yyyy/MM/dd HH:mm')}
-                        </Form.Label>
-                        <InputGroup className="mb-3">
+            <>
+                <Container>
+                    <Form onSubmit={this.createMapping}>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor="target">Target URL</Form.Label>
                             <Form.Control
-                                name="short_url"
-                                type="text"
-                                readOnly
-                                value={this.state.short_url}
+                                name="target"
+                                type="url"
+                                required
+                                placeholder="Enter the URL to shorten..."
+                                value={this.state.target}
+                                onChange={this.onChange}
                             />
-                            <Button
-                                onClick={this.copyToClipboard}
-                                variant="outline-secondary"
-                            >
-                              Copy
-                            </Button>
-                            <Button
-                                href={this.state.short_url}
-                                variant="outline-secondary"
-                                target="_blank"
-                            >
-                              Visit
-                            </Button>
-                        </InputGroup>
+                        </Form.Group>
+                        <Form.Group className="mb-3" hidden={this.state.short_url_hidden}>
+                            <Form.Label htmlFor="short_url">
+                                Here's your shortened URL, valid
+                                until: {format(this.state.expiry_date, 'yyyy/MM/dd HH:mm')}
+                            </Form.Label>
+                            <InputGroup className="mb-3">
+                                <Form.Control
+                                    name="short_url"
+                                    type="text"
+                                    readOnly
+                                    value={this.state.short_url}
+                                />
+                                <Button
+                                    onClick={this.copyToClipboard}
+                                    variant="outline-secondary"
+                                >
+                                    Copy
+                                </Button>
+                                <Button
+                                    href={this.state.short_url}
+                                    variant="outline-secondary"
+                                    target="_blank"
+                                >
+                                    Visit
+                                </Button>
+                            </InputGroup>
 
-                    </Form.Group>
-                    <Button
-                        variant="primary"
-                        type="submit"
-                        disabled={this.state.shorten_disabled}
-                    >
-                        Shorten
-                    </Button>
-                    <Button
-                        className="mx-2"
-                        variant="secondary"
-                        type="button"
-                        onClick={this.resetState}
-                    >
-                        Reset
-                    </Button>
-                </Form>
+                        </Form.Group>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            disabled={this.state.shorten_disabled}
+                        >
+                            Shorten
+                        </Button>
+                        <Button
+                            className="mx-2"
+                            variant="secondary"
+                            type="button"
+                            onClick={this.resetState}
+                        >
+                            Reset
+                        </Button>
+                    </Form>
+                </Container>
 
-                <Toast show={this.state.showCopied} onClose={this.toggleShowCopied}>
-                    <Toast.Header>
-                        <span className="me-auto">Copied to clipboard!</span>
-                    </Toast.Header>
-                </Toast>
-            </Container>
+                <ToastContainer className="p-3" position="top-end">
+                    <Toast
+                        show={this.state.showCopied}
+                        onClose={this.toggleShowCopied}
+                        delay={3000} autohide
+                    >
+                        <Toast.Header>
+                            <span className="me-auto">Copied to clipboard!</span>
+                        </Toast.Header>
+                    </Toast>
+                </ToastContainer>
+            </>
         );
     }
 }
